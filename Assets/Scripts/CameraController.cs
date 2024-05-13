bdb3b7d5c04d35bf9834aa3c18ecf7b0dd0f442d
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-13 21:34:23 +0900

    Feture: 카메라 이동 기능 구현
    Todo: 부드러운 움직임을 위한 보간 사용

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera camera;
+    public Transform chracterPos;
+    public Vector3 offset = new Vector3(0, 0, -10);
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = chracterPos.position + offset;
+        camera.transform.position = newPosition;
+    }
+}
