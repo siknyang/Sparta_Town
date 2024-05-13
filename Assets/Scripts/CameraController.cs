204b9cba97e4a1fa29e03b7e677bb78e9a940a77
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-14 07:13:42 +0900

    Rename: 오타 수정

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Camera camera;
-    public Transform chracterPos;
+    public Transform characterPos;
     public Vector3 offset = new Vector3(0, 0, -10);
 
     private void Awake()
@@ -15,7 +15,7 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
-        Vector3 newPosition = chracterPos.position + offset;
+        Vector3 newPosition = characterPos.position + offset;
         camera.transform.position = newPosition;
     }
 }
