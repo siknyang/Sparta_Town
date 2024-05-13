d7adc95eefb0d4d95d74068aabb4a9fd992184de
siknyang <167048578+siknyang@users.noreply.github.com>
2024-05-14 08:50:38 +0900

    Rename: text 필드명 변경
    주석 추가: Todo: 입력 길이가 9로 고정되는 버그 수정

--- Assets/Scripts/JoinButton.cs
+++ Assets/Scripts/JoinButton.cs
@@ -8,10 +8,12 @@ using UnityEngine.UI;
 public class JoinButton : MonoBehaviour
 {
     public Text text;
-    public void StartGame()
+    public Text inputName;
+
+    public void StartGame() // Todo: 입력 길이가 9로 고정되는 버그 수정
     {
-        Debug.Log("입력 길이: " + text.text.Length);
-        if (text.text.Length >=2 && text.text.Length <= 10)
+        Debug.Log("입력 길이: " + inputName.text.Length);
+        if (inputName.text.Length >= 2 && inputName.text.Length <= 10)
         {
             SceneManager.LoadScene("MainScene");
         }
